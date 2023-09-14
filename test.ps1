48bc821d22bc2f306e55487d6948527c233e7258
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-09-14 10:52:46 -0700

    test for boost binaries

--- test.ps1
+++ test.ps1
@@ -1,9 +1,18 @@
 
 
-$Folder = 'C:\Libraries\boost_1_82_0'
-"Test to see if folder [$Folder]  exists"
-if (Test-Path -Path $Folder) {
-    "Path exists!"
+$BoostFolder = 'C:\Libraries\boost_1_82_0'
+$BinaryFolder32 = 'C:\Libraries\boost_1_82_0\lib32-msvc-14.3'
+$BinaryFolder64 = 'C:\Libraries\boost_1_82_0\lib64-msvc-14.3'
+
+if (Test-Path -Path $BoostFolder) {
+    "Boost version installed"
 } else {
-    "Path doesn't exist."
+    "Boost version missing"
+}
+
+if ((Test-Path -Path $BinaryFolder32) -and (Test-Path -Path $BinaryFolder64)) {
+    "Correct Boost binaries present"
+}
+else {
+    "Correct Boost binaries missing"
 }
